b1019009732e1dd27b184f19cfd4e786f37d2398
ujasmehta <73571708+ujasmehta@users.noreply.github.com>
2026-03-27 19:22:43 +0530

    Import IafSchedule component in AuditSchedule

--- apps/client/src/pages/audit-schedule/AuditSchedule.tsx
+++ apps/client/src/pages/audit-schedule/AuditSchedule.tsx
@@ -1,6 +1,7 @@
 import { useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import KgsSidebar from "./KGSSidebar";
+import KgsSidebar from "./KgsSidebar";
+import IafSchedule from "./pages/audit-schedule/iafSchedule"; // if file is lowercase
 
 type CollapsedSections = {
   details: boolean;
