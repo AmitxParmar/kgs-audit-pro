69cffeca898e22383186ba425b2b785d3b145f57
AmitxParmar <amitparmar901@gmail.com>
2026-04-09 17:57:32 +0530

    remove: dist folder

--- apps/server/dist/middleware/rbac.d.ts
+++
@@ -1,4 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export type Role = 'super_admin' | 'cb_admin' | 'lead_auditor' | 'auditor' | 'staff' | 'accreditation_manager';
-export declare function requirePermission(resource: string, action: string): (req: Request, res: Response, next: NextFunction) => Promise<void | Response<any, Record<string, any>>>;
-//# sourceMappingURL=rbac.d.ts.map
